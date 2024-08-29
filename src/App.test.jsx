cdc77bb10ca72b4610915d2d2e6f36b9617c52d7
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-08-29 10:37:27 -0300

    log roles of component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { logRoles } from "@testing-library/dom";
 
 test("button starts with correct color and text", () => {
-  render(<App />)
+  const {container} = render(<App />)
+  // logRoles(container);
   const buttonElement = screen.getByRole('button', {name: /blue/i})
   expect(buttonElement).toHaveClass("red")
 })
