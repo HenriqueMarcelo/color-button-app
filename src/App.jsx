d650490557b9ee9677a79855abcbdbefd40bce26
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-08-29 12:19:38 -0300

    create component colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red")
+  const nextColor = buttonColor === "red" ? "blue" : "red"
+
   return (
     <div>
-      <button className="red">blue</button>
+      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>{nextColor}</button>
     </div>
   );
 }
