302eb8da51a6e7747319c76fe7c863d7beeeb331
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-08-30 17:23:28 -0300

    test: disable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red")
-  const nextColor = buttonColor === "red" ? "blue" : "red"
+  const [disabled, setDisabled] = useState(false)
+  const [buttonColor, setButtonColor] = useState('red')
+  const nextColor = buttonColor === 'red' ? 'blue' : 'red'
 
   return (
     <div>
-      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>{nextColor}</button>
+      <button disabled={disabled} className={buttonColor} onClick={() => setButtonColor(nextColor)}>
+        {nextColor}
+      </button>
       <br />
-      <input type="checkbox" id="disable-button-checkbox" defaultChecked={false}/>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
       <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,16 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
-import { expect } from "vitest";
+import { render, screen, fireEvent } from '@testing-library/react'
+import App from './App'
+import { expect } from 'vitest'
 
-test("button click flow", () => {
+test('button click flow', () => {
   // render button
   render(<App />)
 
   //find the button
-  const buttonElement = screen.getByRole('button', {name: /blue/i})
+  const buttonElement = screen.getByRole('button', { name: /blue/i })
 
   // check initial color
-  expect(buttonElement).toHaveClass("red")
+  expect(buttonElement).toHaveClass('red')
 
   //click the button
   fireEvent.click(buttonElement)
@@ -19,21 +19,29 @@ test("button click flow", () => {
   expect(buttonElement).toHaveTextContent(/red/i)
 
   // check button color
-  expect(buttonElement).toHaveClass("blue")
+  expect(buttonElement).toHaveClass('blue')
 })
 
-test("checkbox flow", () => {
-  render(<App/>)
+test('checkbox flow', () => {
+  render(<App />)
 
   // find elements
-  const buttonElement = screen.getByRole("button", {name: /blue/i})
-  const checkboxElement = screen.getByRole("checkbox", {name: /disable button/i })
+  const buttonElement = screen.getByRole('button', { name: /blue/i })
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i })
 
   // check initial conditions
   expect(buttonElement).toBeEnabled()
   expect(checkboxElement).not.toBeChecked()
+
+  // check checkbox
+  fireEvent.click(checkboxElement)
+
+  // check if it's changed
+  expect(buttonElement).toBeDisabled()
+
+  // uncheck checkbox
+  fireEvent.click(checkboxElement)
+
+  // check if the button is back to enable
+  expect(buttonElement).toBeEnabled()
 })
-
-
-
-
